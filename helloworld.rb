22f937aab80094aeaafdda3b6e0f421df8bf61c2
Ian moss <ian@alter.is>
2023-01-05 13:00:23 +0100

    add in an initialise that displays an introduction message

--- helloworld.rb
+++ helloworld.rb
@@ -1,6 +1,8 @@
 # Hi from @alterisian in Málaga
 # Lets make the Mexican wave of ruby code.
 #
+# Usage: ruby helloworld.rb
+#
 # Inspired by the likes of Woody Zuil and Bob Allen
 # who popularised mob programming, and introduced the world to
 # global liquid mobs, lets try and create a global mob for ruby
@@ -9,12 +11,16 @@
 # Keep it as simple as possible in January's circumvention of the globe,
 # to make it as easy as possible for the next contributor.
 
-# TODO: February. Write what features you'd like to add below.
+# TODO: February. Write what features you'd like us all to work on below.
 # TODO - @alterisian : Add next_location to calculate where is next WEST from you using the previous contributions as a reference
 
 # Reference: https://twitter.com/alterisian/status/1610941873589477377
 
 class Helloworld
+  def initialize
+    puts "Hi from:"
+  end
+
   def say_hello(person, location)
     puts "#{person}, #{location}"
   end
@@ -22,3 +28,5 @@ end
 
 hi = Helloworld.new
 hi.say_hello("@alterisian", "Málaga, Spain")
+
+#TODO - January - add your call to hi.say_hello above this line. Keep this line.
