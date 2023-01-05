7c346a83b1e7ef949108cac09d6e687f507ea15b
Guillermo Díaz <diazgdev@192.168.0.192>
2023-01-05 11:45:28 -0600

    diazgdev

--- helloworld.rb
+++ helloworld.rb
@@ -33,5 +33,6 @@ end
 hi = Helloworld.new
 hi.say_hello("@alterisian", "Málaga, Spain")
 hi.say_hello("@lucianghinda", "Bucharest, Romania")
+hi.say_hello("@diazgdev", "Aguascalientes, México")
 
 #TODO - January - add your call to hi.say_hello above this line. Keep this line.
