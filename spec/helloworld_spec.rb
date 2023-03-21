375ef552829e848ba8041ca43188dde3ec44c714
Ian Moss <alterisian@users.noreply.github.com>
2023-03-21 17:59:10 +0000

    Integrate VCR into the rest of the tests

--- spec/helloworld_spec.rb
+++ spec/helloworld_spec.rb
@@ -24,31 +24,35 @@ describe Helloworld do
     end
 
     it 'returns three people west of a given person' do
-      helloworld = Helloworld.new
-      helloworld.say_hello("@theOnlyMaDDogx", "New Delhi, India")
-      helloworld.say_hello("@alterisian", "Málaga, Spain")
-      helloworld.say_hello("@bsilva96", "Machalí, Chile")
-      helloworld.say_hello("@lucianghinda", "Bucharest, Romania")
+      VCR.use_cassette("west_of_returns_three_people_west_of_a_given_person") do
+        helloworld = Helloworld.new
+        helloworld.say_hello("@theOnlyMaDDogx", "New Delhi, India")
+        helloworld.say_hello("@alterisian", "Málaga, Spain")
+        helloworld.say_hello("@bsilva96", "Machalí, Chile")
+        helloworld.say_hello("@lucianghinda", "Bucharest, Romania")
 
-      expect( helloworld.west_of("@theOnlyMaDDogx") ).to eq(["@alterisian, Málaga, Spain", "@bsilva96, Machalí, Chile", "@lucianghinda, Bucharest, Romania"])
+        expect( helloworld.west_of("@theOnlyMaDDogx") ).to eq(["@alterisian, Málaga, Spain", "@bsilva96, Machalí, Chile", "@lucianghinda, Bucharest, Romania"])
+      end
     end
 
     it 'generates tweetable output' do
-      helloworld = Helloworld.new
-      helloworld.say_hello("@theOnlyMaDDogx", "New Delhi, India")
-      helloworld.say_hello("@alterisian", "Málaga, Spain")
-      helloworld.say_hello("@bsilva96", "Machalí, Chile")
-      helloworld.say_hello("@lucianghinda", "Bucharest, Romania")
-      people_west_of = helloworld.west_of("@theOnlyMaDDogx")
-      location = "New Delhi, India"
+      VCR.use_cassette("west_of_generates_tweetable_output") do
+        helloworld = Helloworld.new
+        helloworld.say_hello("@theOnlyMaDDogx", "New Delhi, India")
+        helloworld.say_hello("@alterisian", "Málaga, Spain")
+        helloworld.say_hello("@bsilva96", "Machalí, Chile")
+        helloworld.say_hello("@lucianghinda", "Bucharest, Romania")
+        people_west_of = helloworld.west_of("@theOnlyMaDDogx")
+        location = "New Delhi, India"
 
-      expect( helloworld.generate_tweet(people_west_of, location) ).to eq(<<~TWEET
-        Hey @alterisian, @bsilva96, @lucianghinda, #helloworld_rb is almost finished in New Delhi, India.
-        Can we hand over the mob to you?
-        Join https://meet.jit.si/TodayMálagaTomorrowWeMake
-        Please fill out the following form to share your availability: https://forms.gle/BxVGGFqCxJd1i9w88
-      TWEET
-      )
+        expect( helloworld.generate_tweet(people_west_of, location) ).to eq(<<~TWEET
+          Hey @alterisian, @bsilva96, @lucianghinda, #helloworld_rb is almost finished in New Delhi, India.
+          Can we hand over the mob to you?
+          Join https://meet.jit.si/TodayMálagaTomorrowWeMake
+          Please fill out the following form to share your availability: https://forms.gle/BxVGGFqCxJd1i9w88
+        TWEET
+        )
+      end
     end
   end
 end
